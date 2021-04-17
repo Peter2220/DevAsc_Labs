366e38b4061d38b841641203cd7e0a30cbd5f910
Peter Victor <petertherock20@yahoo.com>
2021-04-17 20:40:03 +0200

    Add pprint

--- 2. APIs/Get_Devices.py
+++ 2. APIs/Get_Devices.py
@@ -1,8 +1,14 @@
 import requests
+from pprint import pprint
 from Get_Token import token
 
 URL = 'https://sandboxdnac.cisco.com/api/v1/network-device'
 hdr = {'x-auth-token': token, 'content-type' : 'application/json'}
 resp = requests.get(URL, headers=hdr)
 device_list = resp.json()
-print(device_list)
+
+# Write the output to a file
+with open('Device_list.json', 'wt') as out:
+    pprint(device_list, stream=out)
+
+pprint(device_list)
